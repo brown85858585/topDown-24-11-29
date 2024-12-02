605f33d10343ebc6f53623828799056d8ac7e851
brown85858585 <151971750+brown85858585@users.noreply.github.com>
2024-12-02 16:29:46 +0300

    rb.MovePlayer

--- Assets/scripts_cs/Player.cs
+++ Assets/scripts_cs/Player.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public FixedJoystick fixedJoystick;
+
+    // Privates
     private float _playerSpeed; // Скорочть игрока настраивается в JSON
     private string _horizontal = "Horizontal";
     private string _vertical = "Vertical";
-    // private float _inputHorizontal;
-    // private float _inputVertical;
     private Vector2 _inputMove;
     private Vector2 deltaPosition;
+    private Rigidbody2D rb;
 
     private void Start()
     {
+        StartMaker();
+    }
+    private void StartMaker(){
         _playerSpeed = GameConstReader.gameConstants.player.playerSpeed;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update(){
         InputTaker();
-        MoveNormalize();
         CheckInput();
-        MoveState();
-        NewPosition();
+        // NewPosition();
+    }
+    private void FixedUpdate(){
+        RbMovePosition();
     }
     private void InputTaker(){
         // GetAxisRaw выдает дискретно -1, 0, 1
@@ -30,14 +37,17 @@ public class Player : MonoBehaviour
             Input.GetAxisRaw(_horizontal),
             Input.GetAxisRaw(_vertical)
         );
-        // _inputHorizontal = Input.GetAxisRaw(_horizontal);
-        // _inputVertical = Input.GetAxisRaw(_vertical);
-    }
-    private void MoveNormalize(){
+        // Если схватил за джойстик, то он перезапишет значения
+        if ((fixedJoystick.Vertical != 0) || fixedJoystick.Horizontal != 0){
+        _inputMove = new Vector2(
+            fixedJoystick.Horizontal,
+            fixedJoystick.Vertical
+        );
+        }
         _inputMove.Normalize();
     }
     private void CheckInput(){
-        if (_inputMove != new Vector2(0, 0)){
+        if (_inputMove != Vector2.zero){
             MoveState();
         } else {
             IdleState();
@@ -45,17 +55,32 @@ public class Player : MonoBehaviour
     }
     private void MoveState(){
         deltaPosition = new Vector2(
-            _playerSpeed * Time.deltaTime * _inputMove.x,
-            _playerSpeed * Time.deltaTime * _inputMove.y
+            _playerSpeed * _inputMove.x,
+            _playerSpeed * _inputMove.y
         );
+        HorizonFlipper();
+    }
+    private void HorizonFlipper(){
+        if (_inputMove.x > 0){
+            transform.localScale = new Vector3 (1, transform.localScale.y, transform.localScale.z);
+        } else {
+            transform.localScale = new Vector3 (-1, transform.localScale.y, transform.localScale.z);
+        }
     }
     private void IdleState(){
         deltaPosition = Vector2.zero;
     }
     private void NewPosition() {
+        // Не физичное движение
         transform.position = new Vector2 (
-            transform.position.x + deltaPosition.x,
-            transform.position.y + deltaPosition.y
+            transform.position.x + deltaPosition.x * Time.deltaTime ,
+            transform.position.y + deltaPosition.y * Time.deltaTime 
         );
     }
+    private void RbMovePosition(){
+        // В обучалке движение с физикой. Решил попробовать его
+        rb.MovePosition(rb.position + deltaPosition * Time.fixedDeltaTime);
+    }
 }
+// Довольно просто пишется. Сам в шоке
+// https://t.me/natureModelSpb
